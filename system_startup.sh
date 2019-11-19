3608abc3dcb263004b85a3ce9f0a71455dd59340
Mak Patil <makrandapatil17@gmail.com>
2019-11-18 16:59:55 -0800

    one more command added

--- system_startup.sh
+++ system_startup.sh
@@ -3,5 +3,6 @@ open -a "Slack.app"
 open -a "Visual Studio Code.app"
 #open -a "Visual Studio Code.app" <folder_name>
 open -a "Google Chrome.app" https://www.google.com/
+# open -a "Google Chrome.app" https://www.youtube.com/ https://www.wikipedia.org/ https://www.reddit.com/
 open -a "Sourcetree.app"
 open -a "PhpStorm.app"
