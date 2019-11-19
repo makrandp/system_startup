cec66aee6d0df63ed4e6a8323be9d7f2a96c12fd
Mak Patil <Mak.Patil@glu.com>
2019-11-18 16:55:17 -0800

    added script to start required applications

---
+++ system_startup.sh
@@ -0,0 +1,7 @@
+open -a "Microsoft Outlook.app"
+open -a "Slack.app"
+open -a "Visual Studio Code.app"
+#open -a "Visual Studio Code.app" <folder_name>
+open -a "Google Chrome.app" https://www.google.com/
+open -a "Sourcetree.app"
+open -a "PhpStorm.app"
